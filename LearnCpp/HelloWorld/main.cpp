4a4fca0d533fb1a653569f7f68b0ca3f4b334ba0
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-02-09 06:59:20 +0200

    Update comment

--- LearnCpp/HelloWorld/main.cpp
+++ LearnCpp/HelloWorld/main.cpp
@@ -9,7 +9,7 @@ int main()
 // 1. Refactor this code out into a function that gets called from main
 // 1.1 Where must this function go in the file?
 // 1.2 What should we call the function?
-// 2. Refactor the function created previously so that it can print any string
+// 2. Refactor the function created previously so that it can print any string (don't worry about data types here, just an extra)
 // 2.1 How do we pass the string into the function?
 // 2.2 c string or string object?
 // 2.3 What else can you do to improve the code based on previous issues?
